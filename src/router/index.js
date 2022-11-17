74c01c29be729e12fcc41756f586d1e4ad49236b
topDevfullstack <kalvinson961010@gmail.com>
2022-11-17 04:00:17 -0500

    Add button

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/categories/:slug',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/products/:slug',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
