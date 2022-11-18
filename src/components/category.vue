fc1d45c10c4d072d97002592976930f93eb706a3
topDevfullstack <kalvinson961010@gmail.com>
2022-11-18 07:53:15 -0500

    Complete mounted-lifecylcle

--- src/components/category.vue
+++ src/components/category.vue
@@ -4,6 +4,7 @@
       <div class="col-md-12" v-for="(category, index) in categories" :key="index">
         <div v-if="cateId == category.categoryId">
           <h1>{{ title }} {{ category.categoryTitle }}</h1>
+          <span>{{ totalCount }}Counts {{ totalAmount }}$</span>
         </div>
       </div>
     </div>
@@ -12,7 +13,7 @@
       <div class="col-md-4 col-lg-4" v-for="(data, index) in products" :key="index">
         <h3 @click="goTodetail(data.productId)">{{ data.productTitle }}</h3>
         <img :src="data.image" class="img-fluid">
-        <h4>{{ data.productPrice }}</h4>
+        <h4>{{ data.productPrice }}$</h4>
       </div>
     </div>
     <hr />
@@ -27,6 +28,8 @@ export default {
   name: 'category',
   data () {
     return {
+      totalCount: 0,
+      totalAmount: 0,
       cateId: this.$route.params.Cid,
       title: 'Category',
       categories: [
@@ -43,19 +46,19 @@ export default {
         {
           productTitle: 'ABCN',
           image: require('../assets/logo.png'),
-          productPrice: '100$',
+          productPrice: 100,
           productId: 1
         },
         {
           productTitle: 'KARMA',
           image: require('../assets/logo.png'),
-          productPrice: '100$',
+          productPrice: 200,
           productId: 2
         },
         {
           productTitle: 'Tino',
           image: require('../assets/logo.png'),
-          productPrice: '100$',
+          productPrice: 300,
           productId: 3
         }
       ]
@@ -65,6 +68,16 @@ export default {
     goTodetail (prodId) {
       this.$router.push({ name: 'details', params: { Pid: prodId } })
     }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      // do stuff with the DOM
+      console.log('mounted', this.totalCount)
+      this.totalCount = this.products.length
+      this.products.forEach(product => {
+        this.totalAmount += product.productPrice
+      })
+    })
   }
 }
 </script>
