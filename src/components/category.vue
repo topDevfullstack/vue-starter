fbd371cd28ba64c156e409de0e654f6d7a6b454f
topDevfullstack <kalvinson961010@gmail.com>
2022-11-18 04:49:30 -0500

    Update category

--- src/components/category.vue
+++ src/components/category.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="category">
-    <h1>{{ title }}</h1>
+    <div class="row">
+      <div class="col-md-12" v-for="(category, index) in categories" :key="index">
+        <div v-if="cateId == category.categoryId">
+          <h1>{{ title }} {{ category.categoryTitle }}</h1>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-md-4 col-lg-4" v-for="(data, index) in products" :key="index">
+        <h3 @click="goTodetail(data.productId)">{{ data.productTitle }}</h3>
+        <img :src="data.image" class="img-fluid">
+        <h4>{{ data.productPrice }}</h4>
+      </div>
+    </div>
     <hr />
     <router-link class="nav-link" to="/">
       <button type="button" class="btn btn-primary">Return Home Page</button>
@@ -13,7 +27,43 @@ export default {
   name: 'category',
   data () {
     return {
-      title: 'Category'
+      cateId: this.$route.params.Cid,
+      title: 'Category',
+      categories: [
+        {
+          categoryTitle: 'Battery',
+          categoryId: 1
+        },
+        {
+          categoryTitle: 'Parts',
+          categoryId: 2
+        }
+      ],
+      products: [
+        {
+          productTitle: 'ABCN',
+          image: require('../assets/logo.png'),
+          productPrice: '100$',
+          productId: 1
+        },
+        {
+          productTitle: 'KARMA',
+          image: require('../assets/logo.png'),
+          productPrice: '100$',
+          productId: 2
+        },
+        {
+          productTitle: 'Tino',
+          image: require('../assets/logo.png'),
+          productPrice: '100$',
+          productId: 3
+        }
+      ]
+    }
+  },
+  methods: {
+    goTodetail (prodId) {
+      this.$router.push({ name: 'details', params: { Pid: prodId } })
     }
   }
 }
